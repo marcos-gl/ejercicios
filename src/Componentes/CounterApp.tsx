7fb28b2854fcce5ec0abeafe7b14c1766f55ede8
marcos-gl <marcosgueloz@gmail.com>
2026-07-18 23:43:00 -0600

    update

--- src/Componentes/CounterApp.tsx
+++ src/Componentes/CounterApp.tsx
@@ -1,8 +1,20 @@
+import { useState } from "react"
+
 
 export const CounterApp = () => {
+
+    const [counter,setCounter] = useState(0);
+
+
     return (
-        <>
-            <h1 className="text-white">Componentes</h1>
-        </>
+        <div className="p-4">
+            <h1 className={counter === 0 ? "text-white" : counter < 0 ? "text-red-500" : "text-green-500" }>Cuenta: {counter}</h1>
+
+            <div className="flex gap-2">
+                <button className="bg-gray-200 px-4 py-2 rounded-xl" onClick={() => {if(counter > 0) {setCounter(counter - 1)}}}>Restar</button>
+                <button className="bg-gray-200 px-4 py-2 rounded-xl" onClick={() => setCounter(counter + 1)}>Sumar</button>
+            </div>
+
+        </div>
     )
 }
