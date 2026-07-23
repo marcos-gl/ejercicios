49eec4dc6763668f93f6b5507f2c4234ee850d65
marcos-gl <marcosgueloz@gmail.com>
2026-07-22 22:07:09 -0600

    commit 4

--- src/Componentes/CounterApp.tsx
+++ src/Componentes/CounterApp.tsx
@@ -5,25 +5,14 @@ export const CounterApp = () => {
 
    /*  const [counter,setCounter] = useState(0); */
 
-    const [colors, setColors] = useState("red");
+    const [isVisibile, setIsVisible] = useState(false);
 
 
     return (
-        <div className="flex flex-col content-center">
-          {/*   <h1 className={counter === 0 ? "text-white" : counter < 0 ? "text-red-500" : "text-green-500" }>Cuenta: {counter}</h1>
-
-            <div className="flex gap-2">
-                <button className="bg-gray-200 px-4 py-2 rounded-xl" onClick={() => setCounter(counter > 0 ? counter - 1 : 0)}>Restar</button>
-                <button className="bg-gray-200 px-4 py-2 rounded-xl" onClick={() => setCounter(counter + 1)}>Sumar</button>
-            </div> */}
-
-            <div>
-                <div className={colors === "red" ? "rounded-full bg-red-500 w-16 h-16" : colors === "yellow" ? "rounded-full bg-yellow-500 w-16 h-16" : "rounded-full bg-green-500 w-16 h-16"}></div>
-                <div className="flex gap-2 mt-4">
-                    <button className="bg-gray-200 px-4 py-2 rounded-xl" onClick={() => setColors(colors === "red" ? "green" : colors === "green" ? "yellow" : "red")}>Button</button>
-                </div>
-
-            </div>
+        <div className="flex flex-col items-center">
+         
+            <button className="w-40 h-13 bg-lime-50 rounded-xl text-amber-900" onClick={() => setIsVisible(!isVisibile) } >{isVisibile ? "Ocultar Mnesajes" : "Mostrar mensaje"}</button>
+            {isVisibile && <p className="mt-2 text-amber-900 font-semibold">¡Hola, soy un mensaje secreto!</p>}
 
         </div>
     )
